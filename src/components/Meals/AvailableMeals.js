85e487ec54aaf2ec5210e4f585ceafb2a9833516
Raj Kumar <76458820+stickqz@users.noreply.github.com>
2023-08-01 09:40:27 +0530

    move data to backend

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,78 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+	const [meals, setMeals] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState();
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				"https://react-http-eb29f-default-rtdb.firebaseio.com/meals.json"
+			);
+
+			if (!response.ok) {
+				throw new error("Something went wrong!");
+			}
+			const responseData = await response.json();
+
+			const loadedMeal = [];
+
+			for (const key in responseData) {
+				loadedMeal.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+			// console.log(loadedMeal);
+			setMeals(loadedMeal);
+			setLoading(false);
+		};
+
+		fetchMeals().catch((error) => {
+			setLoading(false);
+			setError(error.message);
+		});
+	}, []);
+
+	if (loading) {
+		return (
+			<section>
+				<p className={classes.loading}>...Loading Meals...</p>
+			</section>
+		);
+	}
+
+	if (error) {
+		return (
+			<section>
+				<p className={classes.error}>Something went wrong!</p>
+			</section>
+		);
+	}
+
+	const mealsList = meals.map((meal) => (
+		<MealItem
+			id={meal.id}
+			key={meal.id}
+			name={meal.name}
+			description={meal.description}
+			price={meal.price}
+		/>
+	));
+
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
+		</section>
+	);
 };
 
 export default AvailableMeals;
