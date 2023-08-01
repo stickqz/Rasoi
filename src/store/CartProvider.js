b4f97990a1f45ab39adfb682d0f16cb61051b8e9
Raj Kumar <76458820+stickqz@users.noreply.github.com>
2023-08-01 09:37:17 +0530

    Update CartProvider.js

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -61,6 +61,11 @@ const cartReducer = (state, action) => {
 			totalAmount: updateTotalAmount,
 		};
 	}
+
+	if (action.type === "CLEAR") {
+		return defaultCartState;
+	}
+
 	return defaultCartState;
 };
 
@@ -77,11 +82,16 @@ const CartProvider = (props) => {
 		dispatchCartAction({ type: "REMOVE", id: id });
 	};
 
+	const clearCartHandler = () => {
+		dispatchCartAction({ type: "CLEAR" });
+	};
+
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItem,
 		removeItem: removeItem,
+		clearCart: clearCartHandler,
 	};
 
 	return (
